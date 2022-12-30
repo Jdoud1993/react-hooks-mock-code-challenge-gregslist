4f50be9d444c89c28bad6ef835357e5bb510952a
Joshua <bigjj3393@yahoo.com>
2022-12-29 22:34:38 -0800

    commit tree 01e19c5e6c0916cb8d804917566802193d921539

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    fetch(" http://localhost:6001/listings")
+      .then((res) => res.json())
+      .then((data) => setListings(data))
+  }, [])
+
+  function handleDeleteListing (deletedListing) {
+    const updatedListings = listings.filter((listing) => listing.id !== deletedListing.id)
+    setListings(updatedListings)
+  }
+
+  function handleSearch (search) {
+    const searchedListings = listings.filter((listing) => listing.description.toLowerCase().includes(search.toLowerCase()));
+    setListings(searchedListings)
+}
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={handleSearch}/>
+      <ListingsContainer listings={listings} handleDeleteListing={handleDeleteListing} />
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,17 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard"
 
-function ListingsContainer() {
+
+
+
+function ListingsContainer({listings, handleDeleteListing}) {
+
+  const listingsToDisplay = listings.map((listing) => <ListingCard key={listing.id} listing={listing} onDeleteListing={handleDeleteListing}/>)
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingsToDisplay}
       </ul>
     </main>
   );
